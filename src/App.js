d8310006609ca89413383c9b81a0335a761fda3f
Axmadullo2000 <axmadullo2000@gmail.com>
2022-05-27 21:57:57 +0500

    Execute of Footer of website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import { Footer } from './Layouts/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to Stack Overflow Clone's website</h2>
+      <Footer />
     </div>
   );
 }
